c5ed7010aa6308bf96957ab9fc5b4bfd23f14f35
jackywu <jacky.wucheng@foxmail.com>
2021-12-06 21:10:35 +0800

    initr

---
+++ src/main/java/com/example/guahao/GuahaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.guahao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuahaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuahaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guahao/GuahaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guahao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuahaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
